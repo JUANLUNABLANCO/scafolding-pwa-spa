916cb8b8199ddf4a778c80a51bfe5b0b06401f34
JUANLUNABLANCO <desarrolloaplicacionesweb.jmlb@gmail.com>
2022-12-05 18:17:05 +0100

    integration tests ok. merging develop branch and feature/formValidation closed

--- cypress/e2e/app/app.integration.cy.js
+++ cypress/e2e/app/app.integration.cy.js
@@ -53,7 +53,7 @@ describe('Router Component Testing: ', () => {
         cy.get('#menu-toggler').click();
         cy.should('not.be.checked');
     });
-    // form
+    // form UI
     it('Nos dirigimos al home, esta prueba es importante hacerla sin llamar al gamePage, al final de la validación del formulario, sino no veremos los resultados en pantalla', () => {
         cy.get('[data-target="home"]').click();
         cy.contains('Home');
@@ -66,5 +66,23 @@ describe('Router Component Testing: ', () => {
         cy.get('#nickName').should('contain', '').and('have.attr', 'required').and('include', 'required');
         cy.get('button[type=submit]').should('contain', 'Let me Play');
 
+    });
+    // form validation 
+    it('Nos dirigimos al home, esta prueba es importante hacerla sin llamar al ROUTER, es decir sin clickar los botones de  navegación de la UI, sino no veremos los resultados en pantalla', () => {
+        cy.get('[data-target="home"]').click();
+        cy.contains('Home');
+    });
+    it('Nos focalizamos en el input name, para introducir datos ficticios correctos JMLB_Moon (datos permitidos), usando la interfaz UI, del formulario, comprobando que el mensaje de error está vacío y la clase que da color verde al borde del iput-control', () => {
+        cy.get('#nickName').should('contain', '').focus().type('JMLB_Moon');
+        cy.get('#submitButton').click();
+        cy.get('.error').first().should('contain', '');
+        cy.get('.input-control').first().should('have.class', 'success')
+
+    });
+    it('Ahora introduciremos datos incorrectos JMLB/1234, comprobando el mensaje de error y la clase que da color rojo al borde del iput-control', () => {
+        cy.get('#nickName').should('contain', '').focus().type('JMLB/1234');
+        cy.get('#submitButton').click();
+        cy.get('.error').first().should('contain', '[a-z][A-z][_]').and('be.visible');
+        cy.get('.input-control').first().should('have.class', 'error');
     });
 });
