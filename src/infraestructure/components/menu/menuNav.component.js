8cd39e62e0d77caf318184b35566bcaa65e50bd7
JUANLUNABLANCO <desarrolloaplicacionesweb.jmlb@gmail.com>
2022-12-06 10:09:56 +0100

    na .

--- src/infraestructure/components/menu/menuNav.component.js
+++ src/infraestructure/components/menu/menuNav.component.js
@@ -2,36 +2,36 @@ import menuNavHTML from './menuNav.component.html';
 import menuNavCSS from './menuNav.component.css';
 
 export class NavigatorMenuComponent extends HTMLElement {
-    constructor () {
-      super();
-    }
-    // static get observerAttributes(){ // lista de atributos a observar
-    //   return [];
-    // }
-    // attributesChangeCallback(nameAtr, oldValue, newValue){ // atributos que podemos controlar
-    //   switch(nameAtr) {
-    //     case 'name':
-    //       this.name = newValue;
-    //       break;
-    //     case "surname":
-    //       this.surname = newValue;
-    //       break;
-    //     case "my-color":
-    //       this.myColor = newValue;
-    //       break;
-    //   }
-    // }
+    constructor() {
+            super();
+        }
+        // static get observerAttributes(){ // lista de atributos a observar
+        //   return [];
+        // }
+        // attributesChangeCallback(nameAtr, oldValue, newValue){ // atributos que podemos controlar
+        //   switch(nameAtr) {
+        //     case 'name':
+        //       this.name = newValue;
+        //       break;
+        //     case "surname":
+        //       this.surname = newValue;
+        //       break;
+        //     case "my-color":
+        //       this.myColor = newValue;
+        //       break;
+        //   }
+        // }
     connectedCallback() { // Cuando se carga el componente, atributos modificables double binding
-      
-      // añadiendole cosas
-      this.id = "menu-navigator";
-      this.innerHTML = menuNavHTML;
+
+        // añadiendole cosas
+        this.id = "menu-navigator";
+        this.innerHTML = menuNavHTML;
     }
-    
+
     // disconnectedCallback() {
-      // metodo que se ejecuta cuando el componente sea eliminado del document
+    // metodo que se ejecuta cuando el componente sea eliminado del document
     // }
-    
+
 }
 // if ('customElements' in window) {
 // 	window.customElements.define("home-page", HomeComponent);
